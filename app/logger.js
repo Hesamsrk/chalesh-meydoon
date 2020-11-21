af7e161124510e7bbc0520acdc321606ac51fd87
Arshak <hesamsrk@gmail.com>
2020-11-21 13:00:19 +0330

    logger url printer : color changed to yellow

--- app/logger.js
+++ app/logger.js
@@ -2,7 +2,7 @@ module.exports = new class logger{
 
 
     requestLogger(req, res,next){
-        console.log(`${req.method} request on ${req.url}`.bold.bgWhite.red);
+        console.log(`${req.method} request on ${req.url}`.bold.yellow);
         next();
     }
 
